29b54273471d9e6d21ad733b75ba2a0a2084c9a9
ralphZero <placideralph@gmail.com>
2022-11-10 16:50:20 -0500

    creating wrapper component

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './src/pages/Home';
-import Explore from './src/pages/Explore';
-import Tickets from './src/pages/Tickets';
-import Settings from './src/pages/Settings';
-import { useFonts } from 'expo-font'
-import { JamIcon } from './src/utils/icons/JamIcon';
+import { useFonts } from 'expo-font';
+import Wrapper from './src/components/Wrapper';
 
 export default function App() {
 
@@ -19,47 +11,9 @@ export default function App() {
     return null;
   }
 
-
-  const Tab = createBottomTabNavigator();
-
-  const options = ({ route }) => ({
-    headerShown: false,
-    tabBarShowLabel: false,
-    tabBarIcon: ({focused, color, size}) => {
-      let iconName;
-      const customColor = focused ? '#3e63f4' : '#333333';
-
-      if(route.name === 'home') {
-        iconName = focused ? 'home-f' : 'home';
-      } else if(route.name === 'explore') {
-        iconName = focused ? 'compass-f' : 'compass';
-      } else if(route.name === 'tickets') {
-        iconName = focused ? 'ticket-f' : 'ticket';
-      } else {
-        iconName = focused ? 'cog-f' : 'cog';
-      }
-      return <JamIcon name={iconName} color={customColor} size={size} />
-    }
-  });
-
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <SafeAreaView style={styles.container}>
-        <Tab.Navigator screenOptions={options}>
-          <Tab.Screen name='home' component={Home} />
-          <Tab.Screen name='explore' component={Explore} />
-          <Tab.Screen name='tickets' component={Tickets} />
-          <Tab.Screen name='settings' component={Settings} />
-        </Tab.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <Wrapper />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+
