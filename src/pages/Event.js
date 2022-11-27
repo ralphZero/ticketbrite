a90989320f21964fb61d8a08558b7c19de6f9b73
ralphZero <placideralph@gmail.com>
2022-11-26 21:37:13 -0500

    building event page

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {ScrollView, Image, StyleSheet, Dimensions} from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet, Dimensions } from 'react-native';
 import FloatingHeader from '../components/event/FloatingHeader';
+import { values } from '../utils/constants';
 
 const Event = (props) => {
     const { route } = props;
@@ -9,9 +10,14 @@ const Event = (props) => {
     return (
         <>
             <FloatingHeader {...props} />
-            <ScrollView style={styles.container}>
-                <Image style={styles.imageBox} source={{uri: url}}/>
-            </ScrollView>
+            <View style={styles.container}>
+                <Image style={styles.imageBox} source={{ uri: url }} />
+                <ScrollView alwaysBounceVertical={false} style={styles.details}>
+                    <View style={styles.detailsContainer}>
+                        <Text style={styles.title}>Artistic Museum 2022</Text>
+                    </View>
+                </ScrollView>
+            </View>
         </>
     );
 }
@@ -24,7 +30,26 @@ const styles = StyleSheet.create({
     imageBox: {
         width: '100%',
         height: Dimensions.get('window').height * 0.5,
+    },
+    details: {
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        paddingTop: Dimensions.get('window').height * 0.38,
+    },
+    detailsContainer: {
+        minHeight: Dimensions.get('window').height * 0.55,
+        backgroundColor: '#fff',
+        borderRadius: '15 15 0 0',
+        padding: values.size.PADDING,
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
     }
+
 })
 
 export default Event;
